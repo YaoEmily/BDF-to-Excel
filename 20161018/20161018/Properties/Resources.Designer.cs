371e85d402c0848914dcadb6ad167d261333fe0e
xuheyao <xuheyao_emily@163.com>
2016-11-12 23:36:04 +0800

    修改名字细节

--- 20161018/20161018/Properties/Resources.Designer.cs
+++ 20161018/20161018/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace _20161018.Properties {
+namespace _ConvertKid.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace _20161018.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("_20161018.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("_ConvertKid.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
